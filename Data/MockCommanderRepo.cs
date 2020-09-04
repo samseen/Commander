56cde0b613ad42ff6a35986c0492e539bd484901
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-04 22:53:33 +0100

    API returns concrete data.

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -9,9 +9,9 @@ namespace Commander.Data
         {
             var commands = new List<Command>
             {
-                new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"};
-                new Command{Id=1, HowTo="Cut bread", Line="Get a knife", Platform="Knife & Chopping Board"};
-                new Command{Id=2, HowTo="Make cup of tea", Line="Place teabag in cup", Platform="Kettle & Cup"};
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle & Pan"},
+                new Command{Id=1, HowTo="Cut bread", Line="Get a knife", Platform="Knife & Chopping Board"},
+                new Command{Id=2, HowTo="Make cup of tea", Line="Place teabag in cup", Platform="Kettle & Cup"}
             };
 
             return commands;

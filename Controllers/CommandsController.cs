2b9ed1b54488ffb906ffa3c388f32b8c36766e4f
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-05 23:14:00 +0100

    Returned DTOs from the command controllers.

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using AutoMapper;
 using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,21 +23,21 @@ namespace Commander.Controllers
 
         //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommands() 
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands() 
         {
             var commandItems =  _repository.GetAllCommands();
 
-            return Ok(commandItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
         //GET api/commands/{id}
         [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int id)
+        public ActionResult <CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
             if(commandItem != null)
             {
-                return Ok(commandItem);
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
             }
             return NotFound();
         }

f5d828f1ebdcbcdc790c189cdf9b4c9dc39edb73
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-05 23:37:44 +0100

    Implemented actions in the interface.

--- Data/SqlCommanderRepo.cs
+++ Data/SqlCommanderRepo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Commander.Models;
@@ -13,6 +14,16 @@ namespace Commander.Data
             _context = context;
         }
 
+        public void CreateCommand(Command cmd)
+        {
+            if(cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            _context.Commands.Add(cmd);
+        }
+
         public IEnumerable<Command> GetAllCommands()
         {
             return _context.Commands.ToList();
@@ -22,6 +33,11 @@ namespace Commander.Data
         {
             return _context.Commands.FirstOrDefault(p =>
                 p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);            
         }
     }
 }

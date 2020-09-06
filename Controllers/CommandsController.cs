a737c409830d8a1016dff72d5b83d89924a209d1
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-06 06:21:15 +0100

    On object creation, the returned resource fits REST specs.

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -31,7 +31,7 @@ namespace Commander.Controllers
         }
 
         //GET api/commands/{id}
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetCommandById")]
         public ActionResult <CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
@@ -50,7 +50,10 @@ namespace Commander.Controllers
             _repository.CreateCommand(commandModel);
             _repository.SaveChanges();
 
-            return Ok(commandModel);
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
+            return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
+            //return Ok(commandReadDto);
         }
     }
 }
